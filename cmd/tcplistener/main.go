13b076a68d28d78a506730fbac0800e2cc48aab8
amaan <amaanmirza287@gmail.com>
2026-03-12 15:17:58 +0530

    Started getting the bytes of data from conn

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+	return out
+}
+func main() {
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatalf("error reading opening file: %v", err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("read: %s\n", line)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amaan287/httpserver
+
+go 1.26.1
